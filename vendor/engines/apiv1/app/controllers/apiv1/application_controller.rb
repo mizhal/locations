28953118dab6b213ff85946404bdfe3e09dee055
mizhal <mizhal@gmail.com>
2017-07-22 14:23:02 +0000

    api fix

--- vendor/engines/apiv1/app/controllers/apiv1/application_controller.rb
+++ vendor/engines/apiv1/app/controllers/apiv1/application_controller.rb
@@ -1,14 +1,14 @@
 module Apiv1
   class ApplicationController < ActionController::API
-    rescue_from 'Api::ParamMissing' do |exception|
+    rescue_from 'Apiv1::ParamMissing' do |exception|
       render json: {error: exception.message}, status: 422
     end
-
+    
     protected 
 
     def required_params *r_par 
       r_par.each do |par|
-        raise Api::ParamMissing.new(par) unless params.has_key?(par)
+        raise Apiv1::ParamMissing.new(par) unless params.has_key?(par)
       end
     end
   end
